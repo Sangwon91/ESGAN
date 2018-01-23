89a06adf356a5e871f494e4967facba0a7a5489e
Sangwon91 <integratedmailsystem@gmail.com>
2018-01-23 14:14:11 +0900

    Upgrade sample generating model.

--- DCGAN/gen.py
+++ DCGAN/gen.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import shutil
 import argparse
 import functools
 
@@ -6,60 +8,123 @@ import numpy as np
 import tensorflow as tf
 
 from model import DCGAN
+from config import (ArgumentParser,
+                    make_arg_parser,
+                    write_config_log,
+                    make_args_from_config)
 from dataset import make_energy_grid_dataset
 
-from utils import write_visit_input
+def prepare_sample_generation(config):
+    """
+    1. Generate sample dir.
+    2. Copy lastest checkpoint.
+
+    """
+    # Get file name (not a path)
+    config_name = config.split("/")[-1]
+    # Get parent path
+    config_folder = "/".join(config.split("/")[:-1])
+    # Extract path
+    date = "-".join(config_name.split("-")[1:])
+
+    # Make sample dir
+    ann_folder = "{}/ann-{}".format(config_folder, date)
+    expression = "{}/save-{}-*".format(config_folder, date)
+    ckpts = glob.glob(expression)
+
+    try:
+        print(">>>>>> Removing folder if exist...")
+        shutil.rmtree(ann_folder)
+    except Exception as e:
+        print(e, "but keep going")
+
+    try:
+        print(">>>>>> Making folder...")
+        os.makedirs(ann_folder)
+    except Exception as e:
+        print(e, "but keep going")
+
+    try:
+        print(">>>>>> Copying ckeckpoint...")
+        for f in ckpts:
+            shutil.copy2(f, ann_folder)
+    except Exception as e:
+        raise Exception(str(e) + ", Terminate program")
+
+    ckpt = ".".join(ckpts[0].split(".")[:-1])
+    ckpt = ckpt.split("/")[-1]
+    ckpt = ann_folder + "/" + ckpt
+
+    return ann_folder, ckpt
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--devices", type=str)
-    parser.add_argument("--ckpt", type=str)
-    parser.add_argument("--sample_dir", type=str)
-    parser.add_argument("--n_samples", type=str)
+    # Custom argparser.
+    gen_parser = ArgumentParser()
+    gen_parser.add_argument("--config", type=str)
+    gen_parser.add_argument("--n_samples", type=str)
+    gen_parser.add_argument("--device", type=str)
 
-    args = parser.parse_args()
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.devices
+    # Parse args for gen.py
+    gen_args = gen_parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = gen_args.device
 
-    energy_scale = [-3000.0, 5000.0]
+    parser = make_arg_parser()
 
+    # Parse original configs
+    args = make_args_from_config(gen_args.config)
+    args = parser.parse_args(args)
+
+    energy_scale = args.energy_scale
+
+    """
     output_writer = functools.partial(
-        write_visit_input, energy_scale=energy_scale)
-
-    dataset = make_energy_grid_dataset(
-        path="/home/FRAC32/PCOD_HIGH",
-        extension="griddata",
-        rotate=False,
-        shape=32,
-        prefetch_size=300,
-        shuffle_size=300,
-        energy_limit=[-3000.0, 5000.0],
+        write_griday_input,
+        invert=args.invert,
         energy_scale=energy_scale,
-        invert=True,
+        cell_length_scale=[0.0, 1.0], # Dummy values.
+        except_grid=True,
+    )
+    """
+
+    # Will not used. but needed in constructor of DCGAN.
+    dataset = make_energy_grid_dataset(
+        path=args.dataset_path,
+        shape=args.voxel_size,
+        invert=args.invert,
+        rotate=args.rotate,
+        move=args.move,
+        extension=args.extension,
+        energy_limit=args.energy_limit,
+        energy_scale=args.energy_scale,
+        shuffle_size=256,
+        prefetch_size=256,
     )
 
     dcgan = DCGAN(
         dataset=dataset,
-        logdir=args.sample_dir,
-        output_writer=output_writer,
-        save_every=100,
-        batch_size=16,
-        z_size=1024,
-        voxel_size=32,
-        bottom_size=4,
-        bottom_filters=256,
-        rate=0.5,
-        top_size=4,
-        filter_unit=32,
-        g_learning_rate=0.0001,
-        d_learning_rate=0.00001,
-        minibatch=False,
-        minibatch_kernel_size=500,
-        minibatch_dim_per_kernel=5,
-        l2_loss=False,
-        train_gen_per_disc=1,
+        logdir=args.logdir,
+        output_writer=None,
+        save_every=args.save_every,
+        batch_size=1,
+        z_size=args.z_size,
+        voxel_size=args.voxel_size,
+        bottom_size=args.bottom_size,
+        bottom_filters=args.bottom_filters,
+        rate=args.rate,
+        top_size=args.top_size,
+        filter_unit=args.filter_unit,
+        g_learning_rate=args.g_learning_rate,
+        d_learning_rate=args.d_learning_rate,
+        minibatch=args.minibatch,
+        minibatch_kernel_size=args.minibatch_kernel_size,
+        minibatch_dim_per_kernel=args.minibatch_dim_per_kernel,
+        l2_loss=args.l2_loss,
+        train_gen_per_disc=args.train_gen_per_disc,
     )
 
-    dcgan.generate_samples(args.sample_dir, args.ckpt, int(args.n_samples))
+    ann_folder, ckpt = prepare_sample_generation(gen_args.config)
+
+    dcgan.generate_samples(ann_folder, ckpt, int(gen_args.n_samples))
 
 
 if __name__ == "__main__":
