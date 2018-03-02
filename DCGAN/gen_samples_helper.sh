ba05cbe72d0cc171e3ca8875797e3d2e75bcdf19
Sangwon91 <integratedmailsystem@gmail.com>
2018-03-02 17:41:00 +0900

    Helper .sh

--- DCGAN/gen_samples_helper.sh
+++ DCGAN/gen_samples_helper.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-TIME="2018-02-27T21:12:05.591534"
+TIME="2018-02-28T21:55:39.687712"
 CONFIG="logdir/ortho/std/config-$TIME"
 FOLDER="logdir/ortho/std/ann-$TIME"
 INFER_CONFIG="logdir/ortho/frac2cell/config-2018-02-21T00:01:05.552610"
 DEVICE=3
-N_SAMPLES=10
+N_SAMPLES=100000
 
 echo Generate griddata from $CONFIG ...
-python interpolate.py --config="$CONFIG" --n_samples=$N_SAMPLES --device=$DEVICE
+python gen.py --config="$CONFIG" --n_samples=$N_SAMPLES --device=$DEVICE
 
 echo Start cell parameter inferences...
 python infer.py --config="$INFER_CONFIG" --griddata_folder="$FOLDER" --device=$DEVICE
 
-echo Make visit inputs...
-cd $FOLDER
-python ~/bin/expand.py *.grid
-make_visit *.grid
+#echo Make visit inputs...
+#cd $FOLDER
+#make_visit *.grid
 
-echo Move visit inputs ...
-mkdir visit
-mv *.bov *.times visit
+#echo Move visit inputs ...
+#mkdir visit
+#mv *.bov *.times visit
