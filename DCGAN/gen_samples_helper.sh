55552401b318e350577c7235511e80f8967ba10a
Sangwon91 <integratedmailsystem@gmail.com>
2018-03-06 11:47:32 +0900

    Change... not important.

--- DCGAN/gen_samples_helper.sh
+++ DCGAN/gen_samples_helper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-TIME="2018-02-28T21:55:39.687712"
-CONFIG="logdir/ortho/std/config-$TIME"
-FOLDER="logdir/ortho/std/ann-$TIME"
-INFER_CONFIG="logdir/ortho/frac2cell/config-2018-02-21T00:01:05.552610"
+TIME="2018-03-02T18:37:26.734375"
+#CONFIG="logdir/ortho/std/config-$TIME"
+#FOLDER="logdir/ortho/std/ann-$TIME"
+CONFIG="logdir/march/config-$TIME"
+FOLDER="logdir/march/ann-$TIME"
+INFER_CONFIG="/home/lsw/Workspace/EGRID_GAN/DCGAN/tensorboard/test/config-2018-03-03T09:59:39.369058"
 DEVICE=3
 N_SAMPLES=100000
 
@@ -11,7 +13,8 @@ echo Generate griddata from $CONFIG ...
 python gen.py --config="$CONFIG" --n_samples=$N_SAMPLES --device=$DEVICE
 
 echo Start cell parameter inferences...
-python infer.py --config="$INFER_CONFIG" --griddata_folder="$FOLDER" --device=$DEVICE
+python infer_cell_resnet.py --config="$INFER_CONFIG" --griddata_folder="$FOLDER" --device=$DEVICE
+#python infer.py --config="$INFER_CONFIG" --griddata_folder="$FOLDER" --device=$DEVICE
 
 #echo Make visit inputs...
 #cd $FOLDER
