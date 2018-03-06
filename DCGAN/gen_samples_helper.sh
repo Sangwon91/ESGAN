c59623bc13aeff82a3e7c65ee02b413993167f8f
Sangwon91 <integratedmailsystem@gmail.com>
2018-03-06 21:01:26 +0900

    gen.sh...

--- DCGAN/gen_samples_helper.sh
+++ DCGAN/gen_samples_helper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TIME="2018-03-02T18:37:26.734375"
+TIME="2018-03-05T23:46:03.784834"
 #CONFIG="logdir/ortho/std/config-$TIME"
 #FOLDER="logdir/ortho/std/ann-$TIME"
 CONFIG="logdir/march/config-$TIME"
 FOLDER="logdir/march/ann-$TIME"
 INFER_CONFIG="/home/lsw/Workspace/EGRID_GAN/DCGAN/tensorboard/test/config-2018-03-03T09:59:39.369058"
 DEVICE=3
-N_SAMPLES=100000
+N_SAMPLES=10000
 
 echo Generate griddata from $CONFIG ...
 python gen.py --config="$CONFIG" --n_samples=$N_SAMPLES --device=$DEVICE
